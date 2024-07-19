0863052c10d03a26de82a63339cfd8807e61109a
WellDN <Wellingtontp0@gmail.com>
2024-07-19 17:11:52 -0300

    first

---
+++ lib/parse.ml
@@ -0,0 +1,12 @@
+module T = struct
+    include Tokens
+end
+
+
+exception ParseError of string
+
+type expected = Tok of T.t | Name of string
+
+let pp_expected fmt = function
+    | Tok tk -> T.pp fmt tk
+    | Name s -> Format.pp_print_string fmt s 
