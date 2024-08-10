e7d350b4517d9bf48578c9e3d4718ecdd855de65
WellDN <Wellingtontp0@gmail.com>
2024-08-10 16:45:18 -0300

    add R10 R11 instructions + tacky for new operators

--- lib/tacky.ml
+++ lib/tacky.ml
@@ -1,12 +1,22 @@
 [@@@coverage exclude_file]
 
 type unary_operator = Complement | Negate [@@deriving show]
+
+type binary_operator = Add | Substract | Multiply | Divide | Mod
+[@@deriving show]
+
 type tacky_val = Constant of int | Var of string [@@deriving show]
 
 type instruction =
     | Return of tacky_val
     | Unary of { op: unary_operator; src: tacky_val; dst: tacky_val }
-    [@@deriving show]
+    | Binary of {
+        op : binary_operator;
+        src1 : tacky_val;
+        src2 : tacky_val;
+        dst : tacky_val;
+    }
+[@@deriving show]
 
 type function_definition =
     | Function of { name: string; body : instruction list }
